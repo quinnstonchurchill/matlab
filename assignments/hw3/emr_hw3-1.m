31ce6b7f3a9cc6bbc13815e56b6536219236e104
Quinn Pruitt <qpruitt93@gmail.com>
2019-02-07 17:26:35 -0500

    problem 1

--- assignments/hw3/emr_hw3-1.m
+++ assignments/hw3/emr_hw3-1.m
@@ -1,28 +1,22 @@
-% Clear screen and any old variables
 clc; clear all;
-% Ask the user for a value they believe is in the Fibonacci sequence
-userValue = input('Please enter a value in the Fibonacci sequence: ');
+guess = input('Enter a value to check: ');
 
-% If the supplied value is negative, display an error message
-if (userValue < 0)
-    fprintf('%d is a negative number and can''t be in the Fibonacci sequence!\n',userValue)
+% don't allow negative numbers
+if (guess < 0)
+    fprintf('%d is a negative number and can''t be in the Fibonacci sequence!\n', guess)
 else
-    % The Fibonacci equation is Fn = Fn-1 + Fn-2
-    % The Fibonacci sequence always starts with 0 and 1
-    first = 0; second = 1; nextInSequence = 0;
-    % Create Fibnoacci numbers until it matches the user's value or 
-    % becomes larger than the user's value
-    while (nextInSequence < userValue)
-        nextInSequence = first+second;
+    first = 0; second = 1; nextNumber = 0;
+    % create next fib number until it matches the guess or is greater than guess
+    while (nextNumber < guess)
+        nextNumber = first+second;
         first = second;
-        second = nextInSequence;
+        second = nextNumber;
     end
-    % Was it actually a Fibonacci number?
-    if (nextInSequence == userValue)
-        fprintf('%d is a Fibonacci number! \n',userValue);
-    % If it wasn't, display the values around the user's value    
+    % check if guess is correct
+    if (nextNumber == guess)
+        fprintf('%d is a Fibonacci number! \n',guess);
+    % show closest numbers
     else
-        fprintf('%d is not a Fibonacci number. \n',userValue);
-        fprintf('The valid numbers around %d are %d and %d \n',userValue,first,second);
+        fprintf('%d is not a Fibonacci number, try %d or %d \n', guess, first, second);
     end
 end

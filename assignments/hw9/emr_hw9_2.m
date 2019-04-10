92c022af9c612ebd86d4e83a658f7a23d4faf539
Quinn Pruitt <qpruitt93@gmail.com>
2019-04-10 19:00:14 -0400

    start

---
+++ assignments/hw9/emr_hw9_2.m
@@ -0,0 +1,11 @@
+% Swap vowels
+% [a e i o u] = [97 101 105 111 117]
+word = 'programming';
+% word = input('Enter the word: ', 's');
+
+% make array of vowels
+for i in length(word)
+  % if letter is vowel
+    % add to array
+end
+
